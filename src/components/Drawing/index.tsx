9e209433f05796af60a3bba125d42734914ad7bf
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2023-02-19 20:55:53 +0000

    word section working

--- src/components/Drawing/index.tsx
+++ src/components/Drawing/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import classes from './Drawing.module.css'
 
 const HEAD = <div className={classes.head} />
--- src/components/Word/index.tsx
+++ src/components/Word/index.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import classes from './Word.module.css'
 
 function index() {
+  const word = 'test'
+  const guessedLetters = ['t']
   return (
-    <div>index</div>
+    <div className={classes.container}>
+      {word.split('').map((letter, index) => (
+        <span className={classes.letterWrapper} key={index}>
+          <span className={guessedLetters.includes(letter) ? classes.letterVisible : classes.letterHidden}>
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
   )
 }
 
